ea61c2aec702051449aaf25d80bdb82be42fb8a7
narendra.vadde <narendra.vadde@Pavans-Mac-mini.local>
2021-02-06 18:38:45 +0530

    logarithmic big-O notation

---
+++ LogarithmicTime.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+import QuartzCore
+
+public class BenchTimer {
+    public static func measureBlock(closer: () -> Void) -> CFTimeInterval {
+        let runCount = 10
+        var executionTimes = Array<Double>(repeating: 0.0, count: runCount)
+        for i in 0..<runCount {
+            let startTime = CACurrentMediaTime()
+            closer()
+            let endTime = CACurrentMediaTime()
+            let execTime = endTime - startTime
+            executionTimes[i] = execTime
+        }
+        return (executionTimes.reduce(0, +) / Double(runCount))
+    }
+}
